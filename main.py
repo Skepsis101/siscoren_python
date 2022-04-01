3b863e1de546302f4e0cadf2d63dec811152553e
Skepsis101 <kevinpatriciosp@gmail.com>
2022-04-01 16:00:12 -0300

    ideias iniciais

---
+++ main.py
@@ -0,0 +1,30 @@
+#RENOVAÇÃO
+    #clicar na inscrição
+    #colar o CPF no campo CPF
+    #apertar ENTER 3 vezes (rapidamente)
+    #clicar na aba REQUERIMENTO
+    #clicar no botão amarelo
+    #clicar em incluir requerimento
+    #rolar duas vezes para baixo
+    #clicar em RENOVAÇÃO DEFINITIVA
+    #clicar em DEFINITIVA
+    #rola duas vezes e selecionar FOTO
+    #rolar 3 vezes e selecionar IDENTIDADE e CÉDULA
+    #clicar em ENVIAR pelo correio
+    #clicar em EMITIR
+    #apertar ENTER e clicar em EMITIR (caso ele peça pelo local de trabalho)
+    #clicar na impressora
+    #apertar enter
+    #fechar a janela
+    #clicar em FECHAR
+    #clicar em DOCUMENTOS
+    #clicar em incluir documento
+    #digitar pasta de ren e apertar TAB
+    #digitar sub e apertar ENTER
+    #apertar TAB e digitar R
+    #clicar em situação e digitar aguardando envio para sede
+    #apertar TAB e digitar subseção de passo fundo
+    #apertar TAB 5 vezes e apertar ENTER
+    #apertar enter 3 vezes
+    #apertar para a esquerda e apertar enter
+    #colocar manualmente a data do boleto e imprimir
